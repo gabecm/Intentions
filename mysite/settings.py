0631c074e80bdaa0d0e1c9db8a45ce33e7e4298d
Gabriel Mallari <gcmallari7@gmail.com>
2021-05-12 13:16:34 -0400

    fix attempt 2

--- mysite/settings.py
+++ mysite/settings.py
@@ -169,5 +169,5 @@ EMAIL_HOST = 'smtp.sendgrid.net'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'apikey'
-EMAIL_HOST_PASSWORD = os.environ.get('SENDGRID_API_KEY')
+EMAIL_HOST_PASSWORD = os.getenv('SENDGRID_API_KEY')
 DEFAULT_FROM_EMAIL = 'gabecmoitproject@gmail.com'
